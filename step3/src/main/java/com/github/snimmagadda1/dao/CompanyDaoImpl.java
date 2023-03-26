ce2c2ed2eef21f9828745cbe6661041d0447069c
snimmagadda1 <saiguy@me.com>
2023-03-26 19:16:14 -0400

    Base for DI

---
+++ step3/src/main/java/com/github/snimmagadda1/dao/CompanyDaoImpl.java
@@ -0,0 +1,25 @@
+package com.github.snimmagadda1.dao;
+
+import java.util.logging.Logger;
+
+import com.github.snimmagadda1.model.Company;
+
+public class CompanyDaoImpl implements CompanyDao {
+
+    private static final Logger logger = Logger.getLogger(CompanyDaoImpl.class.getName());
+
+    @Override
+    public void createCompany(Company company) {
+        logger.info("## DAO ## ---- START - create company");
+
+        logger.info("## DAO ## ---- END - create company");
+    }
+
+    @Override
+    public void updateCompany(Company company) {
+        logger.info("## DAO ## ---- START - update company");
+
+        logger.info("## DAO ## ---- END - update company");
+    }
+    
+}
